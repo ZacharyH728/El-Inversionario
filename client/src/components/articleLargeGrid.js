4fd28b81de09d7909ae2267ed560e3b6b5b43a95
Zachary <zacharyh728@gmail.com>
2024-03-12 18:21:55 -0700

    working on large icon and grid

---
+++ client/src/components/articleLargeGrid.js
@@ -0,0 +1,31 @@
+import ArticleIcon from "../components/articleIcon";
+import { useEffect, useState } from "react"
+
+
+export default function AritcleLargeGrid({ tag }) {
+    const [articles, setArticles] = useState([]);
+
+    tag = tag ? tag : ""
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/page/${tag}`)
+            .then(response => {
+                response.json().then(articles => {
+                    setArticles(articles)
+                });
+            });
+    }, []);
+
+    return (
+        <div>
+            <div className="postHeader">
+                <h1>{!tag ? "Latest Articles" : tag.charAt(0).toUpperCase() + tag.slice(1)}</h1>
+            </div>
+            <div className="posts">
+                {articles.length > 0 && articles.map(article => {
+                    return <ArticleIcon key={article._id} {...article} />;
+                })}
+            </div>
+        </div>
+    );
+}
---
+++ client/src/components/articleLargeIcon.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function ArticleLargeIcon({ article, _id, authors, editors, date_created }) {
+    const tags = article.tags.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' > ');
+    return (
+        < div className="post">
+            <Link className="image" to={`/post/${_id}`} >
+            </Link>
+            <div className="text">
+                <Link className="link" to={`/post/${_id}`} >
+                    <h2>{article.title}</h2>
+                </Link>
+                <div className="info">
+                    <h3 className="tag">{tags}</h3>
+                    <time>{new Date(date_created).toDateString()}</time>
+                </div>
+                <p>{article.summary}</p>
+            </div>
+        </div>
+    );
+}
