8569085f1bad15e305ae40a5a547dc8a6240cd83
ZacharyH728 <zacharyh728@gmail.com>
2024-05-06 00:32:52 -0400

    test works

--- server/debug.js
+++ server/debug.js
@@ -1,15 +1,12 @@
-var MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from 'mongodb';
 
-
-const url = "";
+const uri = "";
 
 try {
     var mongoClient = new MongoClient(uri);
     console.log('Connecting to MongoDB Atlas cluster...');
     await mongoClient.connect();
     console.log('Successfully connected to MongoDB Atlas!');
-
-    return mongoClient;
 } catch (error) {
     console.error('Connection to MongoDB Atlas failed!', error);
     process.exit();
