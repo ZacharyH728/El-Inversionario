158a364de4378f1269a3fd808ad36f1adbfebcdd
ZacharyH728 <zacharyh728@gmail.com>
2024-05-06 00:20:36 -0400

    inital debug commit

---
+++ server/debug.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+const url = "";
+
+export async function connectToCluster(uri) {
+  let mongoClient;
+
+  try {
+      mongoClient = new MongoClient(uri);
+      console.log('Connecting to MongoDB Atlas cluster...');
+      await mongoClient.connect();
+      console.log('Successfully connected to MongoDB Atlas!');
+
+      return mongoClient;
+  } catch (error) {
+      console.error('Connection to MongoDB Atlas failed!', error);
+      process.exit();
+  }
+}
+
+mongoClient = await connectToCluster(urL);
