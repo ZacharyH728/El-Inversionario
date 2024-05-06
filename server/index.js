b4e64164c25dd703a3d362317c058747256e65f7
ZacharyH728 <zacharyh728@gmail.com>
2024-05-06 00:52:30 -0400

    WIP transitioning to mongodb

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Article = require('./models/Article');
-const ImageModel = require('./models/ImageModel')
+// const Article = require('./models/Article');
+// const ImageModel = require('./models/ImageModel')
 const multer = require('multer');
-const { default: mongoose } = require('mongoose');
+// const { default: mongoose } = require('mongoose');
+const {MongoClient} = require('mongodb');
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3")
 const multerS3 = require('multer-s3')
 const fs = require('fs');
@@ -20,170 +21,189 @@ const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const bucket = 'el.inversionario.images';
 const region = 'us-west-1'
 const cloudFront = 'https://d2po7ns1qym4os.cloudfront.net/'
+const client = new MongoClient(url);
 
 
 
 console.log(url)
-mongoose.connect(url, {
-    // tlsCAFile: './global-bundle.pem'
-    // useNewUrlParser: true,
-    tls: true,
-    tlsCAFile: './global-bundle.pem'
-})
-    .catch(e => console.log(e.message));
 
-const s3Client = new S3Client({
-    region: region,
-    credentials: {
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-    }
+async function main(){
+    await client.connect().then(() => {
+        console.log("Connected successfully to server")
+    })
 
-})
+    const db = client.db();
+    const articleCollections = db.collection("Articles"); 
+    const imageCollections = db.collection("imageModel");
 
-const upload = multer({
-    storage: multerS3({
-        s3: s3Client,
-        bucket: 'el.inversionario.images',
-        contentDisposition: 'attachment',
-        acl: 'public-read',
-        serverSideEncryption: 'AES256',
-        key: function (req, file, cb) {
-            console.log(file)
-            cb(null, file.originalname);
+    const s3Client = new S3Client({
+        region: region,
+        credentials: {
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
+        }
+    
+    })
+    
+    const upload = multer({
+        storage: multerS3({
+            s3: s3Client,
+            bucket: 'el.inversionario.images',
+            contentDisposition: 'attachment',
+            acl: 'public-read',
+            serverSideEncryption: 'AES256',
+            key: function (req, file, cb) {
+                console.log(file)
+                cb(null, file.originalname);
+            }
+        })
+    });
+    
+    app.post('/submitArticle', upload.array('Images'), async (req, res) => {
+        res.status(200).send();
+    
+        const cleanedPhotos = [];
+    
+        function findObjectByKeyValue(array, key, value) {
+            for (var i = 0; i < array.length; i++) {
+                console.log("looking at ", array[i][key])
+                if (JSON.parse(array[i])[key] === value) {
+                    return JSON.parse(array[i]);
+                }
+            }
+            return null;
+        }
+    
+        console.log(req.body)
+        console.log(req.files)
+        console.log(req.body['Files'])
+    
+        for (let uploadedImage of req.files) {
+            const postedImage = findObjectByKeyValue(req.body['Files'], 'fileName', uploadedImage.originalname)
+            console.log("posted", postedImage)
+            console.log("uploaded", uploadedImage)
+            // cleanedPhotos.push({ url: uploadedImage.location, name: postedImage.imageName })
+            cleanedPhotos.push({ url: cloudFront + uploadedImage['originalname'].replace(' ', '+'), name: postedImage.imageName })
+        }
+    
+        //TODO find db name
+        await articleCollections.insertOne(
+            {
+                authors: req.body['Author(s)'],
+                editors: req.body['Editor(s)'],
+                // reviewers: req.body['Review(s)'],
+                article: {
+                    title: req.body.Title,
+                    summary: req.body.Summary,
+                    body: req.body.Body,
+                    tags: req.body['Tag(s)'],
+                    pros: req.body['Pros'],
+                    cons: req.body['Cons'],
+                    photos: cleanedPhotos
+                }
+            }
+        ).then(() => {
+            "inserted a document"
+        })
+    
+        // const article = await Article.create({
+        //     authors: req.body['Author(s)'],
+        //     editors: req.body['Editor(s)'],
+        //     // reviewers: req.body['Review(s)'],
+        //     article: {
+        //         title: req.body.Title,
+        //         summary: req.body.Summary,
+        //         body: req.body.Body,
+        //         tags: req.body['Tag(s)'],
+        //         pros: req.body['Pros'],
+        //         cons: req.body['Cons'],
+        //         photos: cleanedPhotos
+    
+        //     }
+        // }).catch(e => console.log(e.message))
+        
+        // console.log(article.article);
+    
+    });
+    
+    app.get('/test', (req, res) => {
+        res.json('Hello World');
+    });
+    
+    app.get("/page", async (req, res) => {
+        const articles = await articleCollections.find({}).toArray();
+        if (articles.length) {
+            res.json(articles);
+        } else {
+            res.status(404).send("The articles you're looking for do not exist");
         }
     })
-});
-
-// const db = mongoose.connection;
-
-// db.on("open", async () => {
-//     console.log(await db.db.listCollections().toArray());
-
-// })
-
-// console.log(mongoose.connection.db.listCollections())
-
-app.post('/submitArticle', upload.array('Images'), async (req, res) => {
-    res.status(200).send();
-
-    const cleanedPhotos = [];
-
-    function findObjectByKeyValue(array, key, value) {
-        for (var i = 0; i < array.length; i++) {
-            console.log("looking at ", array[i][key])
-            if (JSON.parse(array[i])[key] === value) {
-                return JSON.parse(array[i]);
-            }
+    
+    app.get("/page/:tag", async (req, res) => {
+        const { tag } = req.params;
+        const articles = await articleCollections.find({ 'article.tags': tag }).toArray();
+        if (articles.length) {
+            res.json(articles);
+        } else {
+            res.status(404).send("The articles you're looking for do not exist");
         }
-        return null;
-    }
-
-    console.log(req.body)
-    console.log(req.files)
-    console.log(req.body['Files'])
-
-    for (let uploadedImage of req.files) {
-        const postedImage = findObjectByKeyValue(req.body['Files'], 'fileName', uploadedImage.originalname)
-        console.log("posted", postedImage)
-        console.log("uploaded", uploadedImage)
-        // cleanedPhotos.push({ url: uploadedImage.location, name: postedImage.imageName })
-        cleanedPhotos.push({ url: cloudFront + uploadedImage['originalname'].replace(' ', '+'), name: postedImage.imageName })
-    }
-
-
-    const article = await Article.create({
-        authors: req.body['Author(s)'],
-        editors: req.body['Editor(s)'],
-        // reviewers: req.body['Review(s)'],
-        article: {
-            title: req.body.Title,
-            summary: req.body.Summary,
-            body: req.body.Body,
-            tags: req.body['Tag(s)'],
-            pros: req.body['Pros'],
-            cons: req.body['Cons'],
-            photos: cleanedPhotos
-
+    })
+    
+    app.get("/image/:filename", async (req, res) => {
+        const { filename } = req.params;
+        const image = await imageCollections.findOne({ 'filename': filename }).toArray();
+    
+        res.contentType('json');
+    
+        res.send(image);
+    
+        // console.log(new ObjectId(image._id.toString()));
+        // image.read();
+    
+        // gfs.createReadStream(new ObjectId(image._id)).pipe(res);
+    
+        // res.contentType(image.contentType);
+        // gfs.createReadStream(image.filename);
+        // gfs.
+        //     gfs.pipe(res);
+        // const image = await ImageModel.find({});
+        // console.log(image);
+        // res.json(image);
+    })
+    
+    
+    app.get("/article/:id", async (req, res) => {
+        const { id } = req.params;
+        // console.log(await Article.findById(id))
+        const article = await articleCollections.findOne({'article.id': id}).toArray();
+    
+        if (article) {
+            res.json(article);
+        } else {
+            res.status(404).send("Sorry, the article you're looking for doesn't exist");
         }
-    }).catch(e => console.log(e.message))
-
-    console.log("\n");
-
-    // console.log(article.article);
-
-});
-
-app.get('/test', (req, res) => {
-
-    res.json('test ok');
-});
-
-app.get("/page", async (req, res) => {
-    const articles = await Article.find();
-    if (articles.length) {
-        res.json(articles);
-    } else {
-        res.status(404).send("The articles you're looking for do not exist");
-    }
-})
-
-app.get("/page/:tag", async (req, res) => {
-    const { tag } = req.params;
-    const articles = await Article.find({ 'article.tags': tag });
-    if (articles.length) {
-        res.json(articles);
-    } else {
-        res.status(404).send("The articles you're looking for do not exist");
-    }
-})
-
-app.get("/image/:filename", async (req, res) => {
-    const { filename } = req.params;
-    const image = await ImageModel.findOne({ 'filename': filename });
-
-    res.contentType('json');
-
-    res.send(image);
-
-    // console.log(new ObjectId(image._id.toString()));
-    // image.read();
-
-    // gfs.createReadStream(new ObjectId(image._id)).pipe(res);
-
-    // res.contentType(image.contentType);
-    // gfs.createReadStream(image.filename);
-    // gfs.
-    //     gfs.pipe(res);
-    // const image = await ImageModel.find({});
-    // console.log(image);
-    // res.json(image);
-})
+    
+    })
+    
+    //TODO Implement into mongodb
+    // app.get("/tags", async (req, res) => {
+    //     let results = new Set();
+    
+    //     for (let element of (await Article.find().select('article.tags -_id'))) {
+    //         for (let tag of element.article.tags) {
+    //             results.add(tag);
+    //         }
+    //     }
+    //     res.json([...results]);
+    // })
+    
+    app.listen(4000);
+    module.exports = express.Router();
 
 
-app.get("/article/:id", async (req, res) => {
-    const { id } = req.params;
-    // console.log(await Article.findById(id))
-    const article = await Article.findById(id);
+}
 
-    if (article) {
-        res.json(article);
-    } else {
-        res.status(404).send("Sorry, the article you're looking for doesn't exist");
-    }
-
-})
-
-app.get("/tags", async (req, res) => {
-    let results = new Set();
-
-    for (let element of (await Article.find().select('article.tags -_id'))) {
-        for (let tag of element.article.tags) {
-            results.add(tag);
-        }
-    }
-    res.json([...results]);
-})
-
-app.listen(4000);
-module.exports = express.Router();
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
