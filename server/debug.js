01c55893e5cc4b58a2419639a57017fd2af331d8
ZacharyH728 <zacharyh728@gmail.com>
2024-05-06 00:26:30 -0400

    removed function

--- server/debug.js
+++ server/debug.js
@@ -3,20 +3,14 @@ var MongoClient = require('mongodb').MongoClient;
 
 const url = "";
 
-async function connectToCluster(uri) {
-  let mongoClient;
+try {
+    var mongoClient = new MongoClient(uri);
+    console.log('Connecting to MongoDB Atlas cluster...');
+    await mongoClient.connect();
+    console.log('Successfully connected to MongoDB Atlas!');
 
-  try {
-      mongoClient = new MongoClient(uri);
-      console.log('Connecting to MongoDB Atlas cluster...');
-      await mongoClient.connect();
-      console.log('Successfully connected to MongoDB Atlas!');
-
-      return mongoClient;
-  } catch (error) {
-      console.error('Connection to MongoDB Atlas failed!', error);
-      process.exit();
-  }
+    return mongoClient;
+} catch (error) {
+    console.error('Connection to MongoDB Atlas failed!', error);
+    process.exit();
 }
-
-await connectToCluster(urL);
